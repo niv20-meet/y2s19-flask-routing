57e065459c3f32f7d473040818caddc0081c0c8c
myname21-meet <myname21@meet.mit.edu>
2019-07-30 14:11:19 +0300

    asd

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy import Column , Integer , String , Boolean
 
 Base = declarative_base()
 
